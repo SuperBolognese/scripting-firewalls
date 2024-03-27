aef3bdc0c61eab484f3740e7a04f0fd454769d02
Xylophone <66425460+SuperBolognese@users.noreply.github.com>
2024-03-27 16:54:50 +0100

    enlever les mdp et infos perso

--- Checkpoint to Stormshield/variables.py
+++ Checkpoint to Stormshield/variables.py
@@ -2,16 +2,17 @@ from stormshield.sns.sslclient import SSLClient
 import requests, json
 
 #stormshield informations variable
+#change values with your informations
 fw_stormshield = SSLClient(
-    host='10.43.193.169', port=443,
-    user='admin', password='Tibiscuit_666$',
+    host='your_stormshield_IP', port=443,
+    user='admin', password='password',
     sslverifyhost=False
 )
 
 #method to make API calls to the checkpoint management appliance
 #must have enabled API beforehand : https://community.checkpoint.com/t5/API-CLI-Discussion/Enabling-web-api/td-p/32641
 
-CPmgmtIP = '10.43.193.25'
+CPmgmtIP = 'SmartCenter_mgmt_IP'
 
 def api_call(ip_addr, port, command, json_payload, sid):
     url = 'https://' + ip_addr + ':' + str(port) + '/web_api/' + command
@@ -28,5 +29,5 @@ def login(user,password):
     response = api_call(CPmgmtIP, 443, 'login',payload, '')
     return response["sid"]
 
-
-sid = login('admin','admin1')
+#put the SmartCenter credentials here
+sid = login('admin','password')
